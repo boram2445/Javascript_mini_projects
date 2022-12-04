d98e5f2ad23d6f9bcc835688208dbe1846e9a12c
Boram Kim <boram2445@gmail.com>
2022-12-04 20:18:04 +0900

    [12월 4일] web api 복습

--- 1.practice/3.coordinates/main.js
+++ 1.practice/3.coordinates/main.js
@@ -1,27 +1,20 @@
 "use strict";
 
-const target = document.querySelector('.target');
-const coordinate = document.querySelector('.coordinate');
-const lineY = document.querySelector('.line-y');
-const lineX = document.querySelector('.line-x');
+const target = document.querySelector(".target");
+const coordinate = document.querySelector(".coordinate");
+const lineY = document.querySelector(".line-y");
+const lineX = document.querySelector(".line-x");
 const targetRect = target.getBoundingClientRect();
 
-window.addEventListener('mousemove', (event)=>{
-  const x = event.clientX;
-  const y = event.clientY;
-  target.style.transform = `translate(${x-(targetRect.width/2)}px, ${y-(targetRect.width/2)}px)`;
-  lineY.style.transform =`translateY(${y}px)`;
-  lineX.style.transform=`translateX(${x}px)`;
-  coordinate.style.transform = `translate(${x+20}px, ${y+20}px)`;
-  // target.style.top = `${y}px`;
-  // target.style.left = `${x}px`;
-  // lineY.style.top = `${y}px`;
-  // lineX.style.left = `${x}px`;
-  // coordinate.style.top = `${y}px`;
-  // coordinate.style.left = `${x}px`;
-  coordinate.innerHTML = `${y} px ${x} px`;
-})
-
+window.addEventListener("mousemove", (e) => {
+  lineY.style.top = `${e.clientY}px`;
+  lineX.style.left = `${e.clientX}px`;
+  target.style.top = `${e.clientY}px`;
+  target.style.left = `${e.clientX}px`;
+  coordinate.style.left = `${e.clientX}px`;
+  coordinate.style.top = `${e.clientY}px`;
+  coordinate.innerHTML = `${e.clientY}px, ${e.clientX}px`;
+});
 /*
   배운점
   1. 반복되는 값은, 변수로 선언해주자. 
